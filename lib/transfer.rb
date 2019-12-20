7395ea924d00971befba50a30179bd69cccf7a15
David Cantoran <davidcantoran36@gmail.com>
2019-12-20 00:24:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,9 @@
 class Transfer
-  
+  attr_accessor :sender, :recipient, :amount
   def initialize(sender, recipient, amount)
-    
+    @sender = sender 
+    @recipient = recipient
+    @amount = amount
   end 
   
 end
