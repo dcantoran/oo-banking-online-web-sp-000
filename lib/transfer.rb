4d593ffa3addaab31de8f797d0e080b9d3aa4c02
David Cantoran <davidcantoran36@gmail.com>
2019-12-20 01:03:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else 
-      "rejected"
+      "Transaction rejected. Please check your account balance."
     end  
   end 
   
