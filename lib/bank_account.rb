3684b977b0101689b5bfe66a146d7f8e0cb74999
David Cantoran <davidcantoran36@gmail.com>
2019-12-20 00:13:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,6 @@
 class BankAccount
-  attr_accessor :status
+  attr_accessor :balance, :status
   attr_reader :name 
-  attr_writer :balance
   def initialize(name)
     @name = name 
     @balance = 1000
